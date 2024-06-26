695b78431f2efb4df0dadc3e8b8ee97db148340d
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-06-26 14:53:58 +0000

    auto_typing

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,42 @@
 function menu() {
-    document.getElementById("dropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
+const digitando = document.querySelector('[data-js="digitando"]');
+
+const mensagens = ['Estudante de Ciência da Computação', 'Desenvolvedora Fullstack'];
+
+let mensagemIndex = 0;
+let caractereIndex = 0;
+let mensagemAtual = '';
+let caractereAtual = '';
+
+const type = () => {
+    if (mensagemIndex === mensagens.length) {
+        mensagemIndex = 0;
+    }
+
+    mensagemAtual = mensagens[mensagemIndex];
+    caractereAtual = mensagemAtual.slice(0, caractereIndex++);
+    digitando.textContent = caractereAtual;
+
+    if (caractereAtual.length === mensagemAtual.length) {
+        mensagemIndex++;
+        caractereIndex = 0;
+    }
+}
+
+setInterval(type, 100)
